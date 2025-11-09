d5413b29c9cb6e791823457caa5b9525d65321ef
ericw <ericwang030@gmail.com>
2025-11-09 12:20:04 -0500

    removed redundancy in authenticate JWT call in /routes/users.js

--- project_root/backend/routes/user.js
+++ project_root/backend/routes/user.js
@@ -8,26 +8,45 @@ const managerController = require('../controllers/managerController');
 const adminController = require('../controllers/adminController');
 const auth = require('../middleware/auth'); // use existing authorization middleware
 
-// static routes
-router.get('/me', auth.authenticate, userController.getMe); 
+// // static routes
+// router.get('/me', auth.authenticate, userController.getMe); 
 
-// ... room for other routes
+// // ... room for other routes
+
+// // protected routes for cashier auth or above
+// router.patch('/points', authenticateJWT, requireCashier, cashierController.addPoints);
+// router.post('/verify-image', authenticateJWT, requireCashier, cashierController.verifyImage);
+// router.patch('/validity-percentage', authenticateJWT, requireCashier, cashierController.updateValidityPercentage);
+
+// // protected routes for manager auth or above
+// router.get('/users', authenticateJWT, requireManager, managerController.getAllUsers);
+// router.get('/credentials', authenticateJWT, requireManager, managerController.getUserCredentials);
+// router.get('/organizations', authenticateJWT, requireManager, managerController.getAllOrganizations);
+// router.patch('/promote', authenticateJWT, requireManager, managerController.promoteUser);
+// router.patch('/demote', authenticateJWT, requireManager, managerController.demoteUser);
+
+// // protected routes for admin auth or above
+// router.patch('/admin/promote', authenticateJWT, requireAdmin, adminController.promoteToManager);
+// router.patch('/admin/demote', authenticateJWT, requireAdmin, adminController.demoteManager);
+
+// static routes
+router.get('/me', authenticateJWT, userController.getMe); // only authenticate
 
 // protected routes for cashier auth or above
-router.patch('/points', authenticateJWT, requireCashier, cashierController.addPoints);
-router.post('/verify-image', authenticateJWT, requireCashier, cashierController.verifyImage);
-router.patch('/validity-percentage', authenticateJWT, requireCashier, cashierController.updateValidityPercentage);
+router.patch('/points', requireCashier, cashierController.addPoints);
+router.post('/verify-image', requireCashier, cashierController.verifyImage);
+router.patch('/validity-percentage', requireCashier, cashierController.updateValidityPercentage);
 
 // protected routes for manager auth or above
-router.get('/users', authenticateJWT, requireManager, managerController.getAllUsers);
-router.get('/credentials', authenticateJWT, requireManager, managerController.getUserCredentials);
-router.get('/organizations', authenticateJWT, requireManager, managerController.getAllOrganizations);
-router.patch('/promote', authenticateJWT, requireManager, managerController.promoteUser);
-router.patch('/demote', authenticateJWT, requireManager, managerController.demoteUser);
+router.get('/users', requireManager, managerController.getAllUsers);
+router.get('/credentials', requireManager, managerController.getUserCredentials);
+router.get('/organizations', requireManager, managerController.getAllOrganizations);
+router.patch('/promote', requireManager, managerController.promoteUser);
+router.patch('/demote', requireManager, managerController.demoteUser);
 
 // protected routes for admin auth or above
-router.patch('/admin/promote', authenticateJWT, requireAdmin, adminController.promoteToManager);
-router.patch('/admin/demote', authenticateJWT, requireAdmin, adminController.demoteManager);
+router.patch('/admin/promote', requireAdmin, adminController.promoteToManager);
+router.patch('/admin/demote', requireAdmin, adminController.demoteManager);
 
 // future parametized routes
 
